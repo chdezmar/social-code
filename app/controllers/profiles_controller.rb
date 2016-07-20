8d9cf7e235a5bce54a8216cac1fa1845fa6e6dba
Carlos <chdezmar@gmail.com>
2016-07-20 10:07:37 +0100

    indentation

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,34 +5,25 @@ class ProfilesController < ApplicationController
     @profile = Profile.find_by(user_id: current_user.id)
   end
 
+  def edit
+  end
 
   def show
     @user = current_user
     @profile = Profile.find_by(user_id: current_user.id)
   end
 
-  # def edit
-	# 	@profile = Profile.find(params)
-	# 	if current_user.id != @profile.user_id
-	# 		flash[:notice] = "Only owner can edit"
-	# 		redirect_to '/'
-	# 	else
-	# 		render user_profile_path
-	# 	end
-	# end
-
-
-	def update
+  def update
     @user = current_user
     @profile = Profile.find_by(user_id: current_user.id)
-		if current_user.id != @profile.user_id
-			flash[:notice] = "Only owner can update"
-			redirect_to '/'
-		else
-			@profile.update(profile_params)
-			redirect_to user_profile_path
-		end
-	end
+    if current_user.id != @profile.user_id
+      flash[:notice] = "Only owner can update"
+      redirect_to '/'
+    else
+      @profile.update(profile_params)
+      redirect_to user_profile_path
+    end
+  end
 
   private
 
