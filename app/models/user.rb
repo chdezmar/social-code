6ebcade30e33fffea7d93576d12b96527767e480
Carlos <chdezmar@gmail.com>
2016-07-20 10:26:41 +0100

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  has_one :profile
+        :recoverable, :rememberable, :trackable, :validatable
+  has_one :profile, dependent: :destroy
   before_create :build_profile
   accepts_nested_attributes_for :profile
 end
