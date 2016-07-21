a7c79c5020b3dab66c12a5894150341b24072a9b
Rb2030 <rossmail2050@gmail.com>
2016-07-21 16:18:22 +0100

    Initiating omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,20 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :trackable, :validatable
+        :recoverable, :rememberable, :trackable, :validatable, :omniauthable
   has_one :profile, dependent: :destroy
 
   has_many :location_managers
   has_many :places , through: :location_managers
-  
+
   before_create :build_profile
   accepts_nested_attributes_for :profile
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -58,7 +58,7 @@ Devise.setup do |config|
   # given strategies, for example, `config.params_authenticatable = [:database]` will
   # enable it only for database (email + password) authentication.
   # config.params_authenticatable = true
-
+config.omniauth :github, '8c99208481e0e5a2ab91', 'c36d44d4c682f6fc2f6a82e21c0f5827df3bb156', :scope => 'user:email'
   # Tell if authentication through HTTP Auth is enabled. False by default.
   # It can be set to an array that will enable http authentication only for the
   # given strategies, for example, `config.http_authenticatable = [:database]` will
