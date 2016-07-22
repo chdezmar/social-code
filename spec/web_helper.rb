09a97b49041b6d0ead9a1e6b3eb8784a104e28b9
toby676 <tjcmui@gmail.com>
2016-07-22 09:57:22 +0100

    unit testing

--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -13,3 +13,17 @@ def sign_up_user(email = 'test@gmail.com', password = '12345678', password_confi
     fill_in 'Password confirmation', with: password_confirmation
     click_button 'Sign up'
 end
+
+def load_location_manager
+  Place.create(id: 1, name: 'Hotbox')
+  Place.create(id: 2, name: 'Hotbox')
+  User.create(id: 1, email: 'one@example.com', password: 'test123')
+  User.create(id: 2, email: 'two@example.com', password: 'test123')
+  User.create(id: 3, email: 'three@example.com', password: 'test123')
+  User.create(id: 4, email: 'four@example.com', password: 'test123')
+  User.create(id: 5, email: 'five@example.com', password: 'test123')
+  locationmanager.create(place_id: 1, user_id: 1)
+  @test_location = locationmanager.create(place_id: 1, user_id: 2)
+  locationmanager.create(place_id: 2, user_id: 3)
+  locationmanager.create(place_id: 2, user_id: 4)
+end
